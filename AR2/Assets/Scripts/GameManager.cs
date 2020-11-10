40859a4c9cfd02fed59e29b87bb289bf40a241b9
jessica880512 <55180625+jessica880512@users.noreply.github.com>
2020-11-10 12:12:12 +0800

    11/10

---
+++ AR2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Animator aniRed;
+    public FixedJoystick joystick;
+    private void Start()
+    {
+        print("開始事件");
+    }
+
+    
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+    }
+
+    public void Playamimation(string aniname) 
+    {
+        print(aniname);
+        aniRed.SetTrigger(aniname);
+        
+        
+    }
+
+}
